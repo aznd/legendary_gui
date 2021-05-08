021531fe79eb4ed2fde73d9d3c19e31d1ff44f65
azndd <jakob.stechow@web.de>
2021-05-08 17:21:56 +0200

    updattee

--- sqlitee.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-def sqlite():
-    global conn
-    conn = sqlite3.connect('data.db')
-    conn.commit
-
-# conn.execute("INSERT INTO games(gamename, codename, runner) VALUES ('HITMAN', 'Barbet', '/usr/bin/wine')")
