b00f0a0ac948cb4419115c6ee63f11f48eab122c
aznd <jakob.stechow@protonmail.com>
2022-01-19 20:52:32 +0100

    More lint cleanup

--- menubar_settings.py
+++ menubar_settings.py
@@ -24,36 +24,55 @@ def topwindow_settings():
     settings_window.geometry('800x600')
 
     label_authentication_section = tk.Label(
-        settings_window, text="Connect your Epic Games Account", anchor=tk.W, bg=color_light_black, fg=color_white)
+        settings_window,
+        text="Connect your Epic Games Account",
+        anchor=tk.W,
+        bg=color_light_black,
+        fg=color_white)
     label_authentication_section.grid(row=0, column=0)
 
     label_text = tk.Label(
-        settings_window, text="Paste the token here", anchor=tk.W, bg=color_light_black, fg=color_white)
+        settings_window,
+        text="Paste the token here",
+        anchor=tk.W, bg=color_light_black,
+        fg=color_white)
     label_text.grid(row=1, column=0)
     hyperlink = tk.Label(
-        settings_window, text="Click here to get your token", bg=color_light_black, fg=color_light_blue, cursor="hand2", anchor=tk.W)
+        settings_window,
+        text="Click here to get your token",
+        bg=color_light_black, fg=color_light_blue,
+        cursor="hand2", anchor=tk.W)
     hyperlink.grid(row=2, column=0)
     hyperlink.bind(
-        "<Button-1>", lambda e: callback("https://www.epicgames.com/id/api/redirect"))
+        "<Button-1>",
+        lambda e: callback("https://www.epicgames.com/id/api/redirect"))
 
     submit_button = tk.Button(
-        settings_window, text="Submit", command=auth_button, bg=color_light_black, anchor=tk.W, fg=color_white)
+        settings_window,
+        text="Submit",
+        command=auth_button,
+        bg=color_light_black,
+        anchor=tk.W, fg=color_white)
     submit_button.grid(row=4, column=0)
     entry_for_token = tk.Entry(
         settings_window, bg=color_light_black, fg=color_white)
     entry_for_token.grid(row=3, column=0)
-    
-    # NEW WAY TO CHECK IF USER IS ALREADY LOGGED IN
-    auth_check = os.path.isfile('/home/jack/.config/legendary/user.json')
-    if auth_check == True:
-        auth_already_complete = tk.Label(
-            settings_window, bg=color_light_black, text="You are already connected with your Epic Games Account!", fg=color_white)
-        auth_already_complete.grid(row=5, column=0)
-    elif auth_check == False:
-        auth_not_complete = tk.Label(
-            settings_window, text="You are currently not connected to a Epic Games Account!", bg=color_light_black, fg=color_white)
-        auth_not_complete.grid(row=5, column=0)
 
+    # NEW WAY TO CHECK IF USER IS ALREADY LOGGED IN
+    auth_check = os.path.isfile('~/.config/legendary/user.json')
+    if auth_check:
+        auth_already_complete = tk.Label(
+            settings_window,
+            bg=color_light_black,
+            text="You are already connected with your Epic Games Account!",
+            fg=color_white)
+        auth_already_complete.grid(row=5, column=0)
+    elif auth_check is False:
+        auth_not_complete = tk.Label(
+            settings_window,
+            text="You are currently not connected to a Epic Games Account!",
+            bg=color_light_black, fg=color_white)
+        auth_not_complete.grid(row=5, column=0)
 
     button_change_runner = tk.Button(
         settings_window, text="Click", command=lambda: change_globalrunner())
@@ -64,8 +83,6 @@ def topwindow_settings():
 
 
 # CHANGE THE CURRENT RUNNER
-
-
 def auth_button():
     global entry
     global entry_for_token
@@ -85,7 +102,8 @@ def auth_button():
         auth_not_succesful_label = tk.Label(auth_not_succesful,
                                             text="Something went wrong")
         auth_not_succesful_label.grid(row=0, column=0)
-        auth_not_succesful_try_again = tk.Button(auth_not_succesful, text="Try Again",
+        auth_not_succesful_try_again = tk.Button(auth_not_succesful,
+                                                 text="Try Again",
                                                  command=lambda: topwindow_settings())
         auth_not_succesful_try_again.grid(row=1, column=0)
 
@@ -95,8 +113,7 @@ def change_globalrunner_submit_button_func():
     print(input_entry_change_globalrunner)
     topwindow_change_runner.destroy()
     data = str(input_entry_change_globalrunner)
-    sql_update_query = "UPDATE games WHERE gamename = 'globalrunner' SET runner (?);", (
-        data)
+    sql_update_query = f"UPDATE games WHERE gamename = 'globalrunner' SET runner ({data});"
     # "INSERT INTO games(gamename, codename) VALUES (?,?);", (title, app_id))
 
     database_sql.cur.execute(sql_update_query)
@@ -114,5 +131,6 @@ def change_globalrunner():
     entry_change_runner = tk.Entry(topwindow_change_runner)
     entry_change_runner.grid(row=1, column=0)
     submit_button_change_runner = tk.Button(
-        topwindow_change_runner, text="Submit", command=lambda: change_globalrunner_submit_button_func())
+        topwindow_change_runner, text="Submit",
+        command=lambda: change_globalrunner_submit_button_func())
     submit_button_change_runner.grid(row=2, column=0)
